6eb903094a16991613416b5264925898b556a6be
MH Dip <mhdip05@outlook.com>
2024-07-13 14:58:50 -0600

    Carsties Front end
    Filtering Functionality

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,12 @@
 import type { Config } from "tailwindcss";
+import flowbite from "flowbite-react/tailwind";
 
 const config: Config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    flowbite.content(),
   ],
   theme: {
     extend: {
@@ -15,6 +17,13 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  corePlugins: {
+    aspectRatio: false,
+  },
+  plugins: [
+    require("@tailwindcss/aspect-ratio"),
+    flowbite.plugin(),
+
+  ],
 };
 export default config;
